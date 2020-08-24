413ee5cbbed24df9cd1679dae6194ce5fe31735a
AYenumula <45498637+AYenumula@users.noreply.github.com>
2020-08-23 17:11:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,25 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+/* Alumni Search */
+
+function myFunction() {
+    var input, filter, cards, cardContainer, h2, title,year, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-body h2.card-title");
+        year = cards[i].querySelector(".card-body p.title");
+        if (year.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
