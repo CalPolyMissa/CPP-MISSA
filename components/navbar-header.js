dbb4768f658d2648312387285f17c9e12de6ad06
turtlelover56 <anadavis@gmail.com>
2025-04-03 16:56:36 -0700

    Created contact-footer component

--- components/navbar-header.js
+++ components/navbar-header.js
@@ -2,32 +2,34 @@
 // Fetch the navbar-header html content.
 fetch("components/navbar-header.html")
   .then(stream => stream.text())
-  .then(text => define(text));
+  .then(text => defineNavbarHeader(text));
 
 // Define the navbar-header after fetching.
-function define(html) {
+function defineNavbarHeader(html) {
   class NavbarHeader extends HTMLElement {
     constructor() {
-    super();
+      super();
+    }
 
-    // Add the "navbar-header" html to the document.
-    const navbarHeaderTemplate = document.createElement("template");
-    navbarHeaderTemplate.innerHTML = html;
-    this.appendChild(navbarHeaderTemplate.content);
-    
-    // Get the "navbar-burger" element.
-    const el = document.getElementById("navbar-header-burger");
+    connectedCallback() {
+      // Add the "navbar-header" html to the document.
+      const navbarHeaderTemplate = document.createElement("template");
+      navbarHeaderTemplate.innerHTML = html;
+      this.appendChild(navbarHeaderTemplate.content);
+      
+      // Get the "navbar-burger" element.
+      const el = document.getElementById("navbar-header-burger");
 
-    // Add "click" event listener to "navbar-burger".
-    el.addEventListener('click', () => {
-      // Get the target from the "data-target" attribute.
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
+      // Add "click" event listener to "navbar-burger".
+      el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute.
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu".
-      el.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
-    });
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu".
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
     }
   }
 
