6a190401c5203a051dd95660fbf5da7267e24709
turtlelover56 <anadavis@gmail.com>
2026-02-09 19:41:58 -0800

    Eboard application link and timer

---
+++ scripts/eboard-timer.js
@@ -0,0 +1,23 @@
+let eboardTimer = setInterval(function() {
+  // Get application open and close dates.
+  let closeDate = new Date("Feb 28, 2026 23:59:59").getTime();
+  let openDate = new Date("Feb 6, 2026 00:00:00").getTime();
+
+  let now = new Date().getTime();
+  let distanceClose = closeDate - now;
+  let distanceOpen = openDate - now;
+
+  // Check if close date has passed.
+  if (distanceClose < 0) {
+    let button = document.getElementById("eboard-button");
+    button.toggleAttribute("disabled", true)
+    button.removeAttribute('href');
+    clearInterval(eboardTimer);
+  }
+  // Else check if open date has passed.
+	else if (distanceOpen < 0) {
+    let button = document.getElementById("eboard-button");
+    button.removeAttribute('disabled');
+    button.href = "https://forms.gle/rNoEW1iZAs4qqqpc6";
+  }
+}, 1000);
